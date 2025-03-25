9a319fca6517c5bfbb21e220a735fc6336c31f03
nsulonen <niko.sulonen@proton.me>
2025-03-25 20:30:50 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame # type: ignore
+from pygame import SurfaceType
+from constants import *
+
+def main() -> None:
+    
+    pygame.init()
+    screen: SurfaceType = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        
+        screen.fill(color=(0,0,0))
+        
+        pygame.display.flip()
+    
+
+
+if __name__ == "__main__":
+    main()
